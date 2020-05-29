2cb125d28094d93f71dbfc925c630534fefc6463
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-29 03:03:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require "pry"
 
 def reverse_each_word(string)
 words=[]  
-  string.split(" ")
-  string.each do |word|
+  array=string.split(" ")
+  array.each do |word|
     words << word.reverse
   end
 puts words
